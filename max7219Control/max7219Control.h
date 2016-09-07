fa95d8a8949200e1fc9da98461c1bd54e44079fd
Paul Newman <picopeck@users.noreply.github.com>
2016-09-07 14:29:49 +0100

    update to reflect function additions
    initDevices and IBIT added to .cpp

--- max7219Control/max7219Control.h
+++ max7219Control/max7219Control.h
@@ -8,15 +8,14 @@ class PImax7219Control
 {
 public:
 	PImax7219Control(int dataPin, int clkPin, int csPin, int numDevices = 1);
-	void testDisplay();
+	void initDevices(); // simply initialises the devices
+	void IBIT(); // sets all digits and decimals to on.
 	void displayNumber(int fDeviceID, long fNumber, int fStartIndex = 1, int fLength = 1, int fPower = 0, int fNegative = 1, bool fLeadingZero=true,bool fDEBUG = false);
 
 private:
 	LedControl _deviceHandle;
 	int DELAY_TIME_uS=1000;
 	int _numDevices;
-	void main();
-
 };
 
 #endif
